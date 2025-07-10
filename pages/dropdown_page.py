f234f9b16e4ef50aa62cd57393e6a9a9bb9a2b4d
rubinagill <msrubinagill@gmail.com>
2025-07-10 10:39:38 +0400

    migrating locators to json files

--- pages/dropdown_page.py
+++ pages/dropdown_page.py
@@ -2,4 +2,3 @@ from selenium.webdriver.common.by import By
 
 class DropdownPage:
     URL_KEY = "/dropdown"
-    SELECT_BOX = (By.ID, "dropdown")
--- pages/upload_page.py
+++ pages/upload_page.py
@@ -2,7 +2,3 @@ from selenium.webdriver.common.by import By
 
 class UploadPage:
     URL_KEY = "/upload"
-    FILE_INPUT = (By.ID, "file-upload")
-    SUBMIT_BUTTON = (By.ID, "file-submit")
-    HEADER = (By.TAG_NAME, "h3")
-    RESULT_FILENAME = (By.ID, "uploaded-files")
